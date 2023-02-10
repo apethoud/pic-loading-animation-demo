66b97c71da86b1e13ccbf4ec6fc03b9df5eb23cf
Andrew Pethoud <andrewpethoud@gmail.com>
2023-02-10 12:14:03 -0800

    add RN Animated opacity example

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useRef } from 'react';
+import { Animated, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const pulseAnim = useRef(new Animated.Value(0)).current;
+
+  const pulse = () => {
+    Animated.timing(pulseAnim, {
+      toValue: 1,
+      duration: 5000,
+      useNativeDriver: true,
+    }).start();
+  };
+
+  useEffect(() => {
+    pulse();
+  }, [])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Animated.View style={[styles.imagePlaceholder, { opacity: pulseAnim }]} />
     </View>
   );
 }
@@ -17,4 +30,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  imagePlaceholder: {
+    width: 150,
+    height: 150,
+    borderRadius: 75,
+    backgroundColor: "#fce9ee"
+  }
 });
